389f41384a097f726c95b9fa07673f129fe06dc5
mrmartin <theophilusmartin123@gmail.com>
2026-01-21 17:05:06 -0600

    Connect to nats server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({})],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres_db',
+      port: 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'users_db',
+      entities: [],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/entity/UsersEntity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName?: string;
+
+  @Column()
+  lastName?: string;
+
+  @Column({ unique: true })
+  email: string;
+  @Column()
+  password: string;
+  @Column()
+  address?: string;
+}
